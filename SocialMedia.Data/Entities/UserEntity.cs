a277a949812fc399660a3b1e4cd901c42eb2a487
hayzemke <hay.zemke@gmail.com>
2022-06-09 19:08:12 -0400

    models added

--- SocialMedia.Data/Entities/UserEntity.cs
+++ SocialMedia.Data/Entities/UserEntity.cs
@@ -1,16 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
     public class UserEntity
-    {
+    {   [Key]
         public int ID { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string ConfirmPassword { get; set; }
-        public DateTime JoinedDate { get; set; }
+
+        public DateTime DateJoined { get; set; }
     }
