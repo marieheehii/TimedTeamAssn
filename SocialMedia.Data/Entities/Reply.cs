4d1e6eaa5392bf1877cd6008d0de600e59f45941
Hannah Young <hamyoun@iu.edu>
2022-06-09 19:43:07 -0400

    added reply

--- SocialMedia.Data/Entities/Reply.cs
+++ SocialMedia.Data/Entities/Reply.cs
@@ -1,9 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
     public class Reply
     {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Comment))]
+        public Comment CommentId { get; set; }
+        [Required]
+        public string Text { get; set; }
         
     }
