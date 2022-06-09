2b6be0b52310e8e8ab6de4f454267725efe33acb
hayzemke <hay.zemke@gmail.com>
2022-06-09 18:32:30 -0400

    post done

--- SocialMedia.Data/Entities/Post.cs
+++ SocialMedia.Data/Entities/Post.cs
@@ -5,5 +5,9 @@ using System.Threading.Tasks;
 
     public class Post
     {
-        
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Like> Likes { get; set; }
     }
