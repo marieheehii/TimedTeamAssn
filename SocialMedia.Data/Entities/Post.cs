bc2e26a19ceccf92acbeb04740ac8829fa2febf0
hayzemke <hay.zemke@gmail.com>
2022-06-09 18:18:17 -0400

    haley branch- UserAdded

--- SocialMedia.Data/Entities/Post.cs
+++ SocialMedia.Data/Entities/Post.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SocialMedia.Data.Entities
-{
     public class Post
     {
         
     }
-}
--- SocialMedia.Data/Entities/UserEntity.cs
+++ SocialMedia.Data/Entities/UserEntity.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SocialMedia.Data.Entities
-{
     public class UserEntity
     {
-        
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public DateTime JoinedDate { get; set; }
     }
-}
